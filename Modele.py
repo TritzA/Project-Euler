ea4d729f05a7253086c65b49db3009ab43238c7e
TritzA <45083857+TritzA@users.noreply.github.com>
2020-06-01 21:10:59 -0400

    Add files via upload

---
+++ Modele.py
@@ -0,0 +1,19 @@
+import time
+import math
+
+
+def fin_temps(temps_initial):
+    temps_final = time.time_ns()
+    return (temps_final - temps_initial) / math.pow(10, 6)
+
+
+# Resume :
+if __name__ == '__main__':
+    temps_initial = time.time_ns()
+
+    ### CODE ICI
+
+    temps_fin = time.time()
+    reponse = -1
+    print("Reponse :", reponse, ", en :", fin_temps(temps_initial), "ms.")
+    # Reponse : -1 , en : -1 ms.
---
+++ Probleme002.py
@@ -0,0 +1,33 @@
+import time
+import math
+
+
+def fin_temps(temps_initial):
+    temps_final = time.time_ns()
+    return (temps_final - temps_initial) / math.pow(10, 6)
+
+
+def est_pair(nombre, multiple=2):
+    return nombre % multiple == 0
+
+
+# Resume : On parcours la suite de Fibonacci dans
+# une boucle allant jusqu'à 4M. Si on nouveau nombre
+# de la suite est paire, on l'ajoute à la somme.
+if __name__ == '__main__':
+    temps_initial = time.time_ns()
+    nb_un = 1
+    nb_deux = 2
+    somme = 2
+
+    while nb_un < 4000000:
+        tempo = nb_un
+        nb_un = nb_deux
+        nb_deux = nb_deux + tempo
+        if est_pair(nb_deux):
+            somme += nb_deux
+
+    temps_fin = time.time()
+    reponse = somme
+    print("Reponse :", reponse, ", en :", fin_temps(temps_initial), "ms.")
+    # Reponse : 4613732 , en : 0.0 ms.
