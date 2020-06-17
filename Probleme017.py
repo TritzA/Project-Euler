603b88da722eaaa61f0de94fcf161c19b5bb74be
Antonin Tritz <45083857+TritzA@users.noreply.github.com>
2020-06-17 13:00:52 -0400

    Résolution du problème 17

---
+++ Probleme017.py
@@ -0,0 +1,64 @@
+import time
+import Utilitaire
+
+
+def unites():
+    somme = len("one")
+    somme += len("two")
+    somme += len("three")
+    somme += len("four")
+    somme += len("five")
+    somme += len("six")
+    somme += len("seven")
+    somme += len("eight")
+    somme += len("nine")
+    return somme * 190
+
+
+def dix_a_dix_neuf():
+    somme = len("ten")
+    somme += len("eleven")
+    somme += len("twelve")
+    somme += len("thirteen")
+    somme += len("fourteen")
+    somme += len("fifteen")
+    somme += len("sixteen")
+    somme += len("seventeen")
+    somme += len("eighteen")
+    somme += len("nineteen")
+    return somme * 10
+
+
+def dizaines():
+    somme = len("twenty")
+    somme += len("thirty")
+    somme += len("forty")
+    somme += len("fifty")
+    somme += len("sixty")
+    somme += len("seventy")
+    somme += len("eighty")
+    somme += len("ninety")
+    return somme * 100
+
+
+def cent():
+    return len("hundred") * 900
+
+
+def et():
+    return len("and") * 891
+
+
+def mille():
+    return len("one") + len("thousand")
+
+
+# Résumé : On calcule la somme des lettres en multipliant la taille des mots par leur nombre d'apparitions.
+if __name__ == '__main__':
+    temps_debut = time.time_ns()
+
+    somme = unites() + dix_a_dix_neuf() + dizaines() + cent() + et() + mille()
+
+    temps_fin = time.time_ns()
+    Utilitaire.afficher_reponse(somme, Utilitaire.fin_temps(temps_debut, temps_fin))
+    # Réponse : 21124 , en : 0 ns.
