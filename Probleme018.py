28d5a3db1ae1b7ae996a6c71adafb23a3734c51b
Antonin Tritz <45083857+TritzA@users.noreply.github.com>
2020-06-17 11:30:07 -0400

    Résolution du problème 18

---
+++ Probleme018.py
@@ -0,0 +1,78 @@
+import time
+import Utilitaire
+
+
+def lire_mat():
+    nombre = "75 " \
+             "95 64 " \
+             "17 47 82 " \
+             "18 35 87 10 " \
+             "20 04 82 47 65 " \
+             "19 01 23 75 03 34 " \
+             "88 02 77 73 07 63 67 " \
+             "99 65 04 28 06 16 70 92 " \
+             "41 41 26 56 83 40 80 70 33 " \
+             "41 48 72 33 47 32 37 16 94 29 " \
+             "53 71 44 65 25 43 91 52 97 51 14 " \
+             "70 11 33 28 77 73 17 78 39 68 17 57 " \
+             "91 71 52 38 17 14 91 43 58 50 27 29 48 " \
+             "63 66 04 68 89 53 67 30 73 16 69 87 40 31 " \
+             "04 62 98 27 23 09 70 98 73 93 38 53 60 04 23 "
+    mat = "mat = ["
+    i = 0
+    while i < 15:
+        if i != 0:
+            mat += ",["
+        else:
+            mat += "["
+        j = 0
+        while j <= i:
+            pos = (i * (i + 1)) // 2 * 3 + j * 3
+            if j != i:
+                mat += str(int(nombre[pos:pos + 2])) + ", "
+            else:
+                mat += str(int(nombre[pos:pos + 2]))
+            j += 1
+        mat += "]"
+        i += 1
+    mat += "]"
+    print(mat)
+
+
+# Résumé : En partant de la dernière ligne, on compare les deux points possibles d'arrivée sur celle-ci
+# en partant de l'avant-dernière ligne. On ajoute ensuite à la dernière ligne la plus grande des deux valeurs possibles.
+# On répète ce processus sur l'entièreté des colonnes de chaque ligne. À la fin, la matrice est complètement transformée
+# et nombre au sommet de la pyramide indique la plus grande somme possible.
+if __name__ == '__main__':
+    temps_debut = time.time_ns()
+
+    # lire_mat()
+    mat = [[75],
+           [95, 64],
+           [17, 47, 82],
+           [18, 35, 87, 10],
+           [20, 4, 82, 47, 65],
+           [19, 1, 23, 75, 3, 34],
+           [88, 2, 77, 73, 7, 63, 67],
+           [99, 65, 4, 28, 6, 16, 70, 92],
+           [41, 41, 26, 56, 83, 40, 80, 70, 33],
+           [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+           [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+           [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+           [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+           [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+           [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
+    ligne = 14
+    while ligne > 0:
+        colonne = 0
+        while colonne < ligne:
+            if mat[ligne][colonne] > mat[ligne][colonne + 1]:  # si le nombre de gauche est le plus grand
+                mat[ligne - 1][colonne] += mat[ligne][colonne]
+            else:  # si le nombre de droite est le plus grand
+                mat[ligne - 1][colonne] += mat[ligne][colonne + 1]
+            colonne += 1
+        ligne -= 1
+
+    temps_fin = time.time_ns()
+    Utilitaire.afficher_reponse(mat[0][0], Utilitaire.fin_temps(temps_debut, temps_fin))
+    # Réponse : 1074 , en : 0 ns.
