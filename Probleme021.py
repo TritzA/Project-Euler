cb17acee34b6f081e147735210086f9587f8aa63
Antonin Tritz <45083857+TritzA@users.noreply.github.com>
2020-06-27 11:33:33 -0400

    Mise à jour

--- Probleme021.py
+++ Probleme021.py
@@ -1,19 +1,19 @@
 import time
-import Utilitaire
+import Uti
+import Nb
 
 # Résumé : On passe à travers les nombres de 2 à 10000
 # exclu en charchant si chaque nombre à un nombre ami.
 if __name__ == '__main__':
-    temps_debut = time.time_ns()
+    temps_debut = time.time()
 
     somme = 0
     a = 2
     while a < 10000:
-        b = Utilitaire.somme_diviseurs(a)
-        if Utilitaire.somme_diviseurs(b) == a and a != b:
+        if Nb.amical(a):
             somme += a
         a += 1
 
-    temps_fin = time.time_ns()
-    Utilitaire.afficher_reponse(somme, Utilitaire.fin_temps(temps_debut, temps_fin))
-    # Réponse : 31626 , en : 9781.6437 ms.
+    temps_fin = time.time()
+    Uti.rep(somme, temps_fin - temps_debut)
+    # Réponse : 31626 , en : 0.244 s.
